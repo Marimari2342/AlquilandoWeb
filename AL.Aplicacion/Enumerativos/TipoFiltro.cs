2b4c48064745297a3d37f836eb2f49a322608b9d
Marimari2342 <marianroj@hotmail.com.ar>
2025-06-03 16:16:23 -0300

    commit arreglé filtrar para que no visualizar los filtros ya aplicados. Eliminé un mensaje de error duplicado.

---
+++ AL.Aplicacion/Enumerativos/TipoFiltro.cs
@@ -0,0 +1,13 @@
+namespace AL.Aplicacion.Enumerativos
+{
+    public enum TipoFiltro
+    {
+        Pais,
+        Ciudad,
+        PrecioMinimo,
+        PrecioMaximo,
+        CantidadDormitorios,
+        Capacidad,
+        Estacionamiento
+    }
+}
